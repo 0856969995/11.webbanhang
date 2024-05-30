ab96c66cab1253f8d82b9b5e8626a4bb34dad273
hoang <hoang@ADMIN>
2024-05-30 20:53:05 +0700

    Nguyen Xuan Hoang

--- src/main/java/com/example/webbanhang/service/CourseService.java
+++ src/main/java/com/example/webbanhang/service/CourseService.java
@@ -1,10 +1,13 @@
 package com.example.webbanhang.service;
 
 import com.example.webbanhang.model.Course;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Service
 public class CourseService {
-    private List<Course> listCourses= ArrayList<>();
+    private List<Course> listCourses= new  ArrayList<>();
+    public void addCourse(Course newProduct) {listCourses.add(newProduct);}
+    public List<Course> GetAll() {return listCourses;}
 }

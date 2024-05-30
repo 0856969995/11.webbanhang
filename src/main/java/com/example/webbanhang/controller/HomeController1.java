5c8738f7d440ca34c64818eb6033665aa743a692
hoang <hoang@ADMIN>
2024-05-30 20:53:21 +0700

    Nguyen Xuan Hoang

--- src/main/java/com/example/webbanhang/controller/HomeController1.java
+++ src/main/java/com/example/webbanhang/controller/HomeController1.java
@@ -1,13 +1,11 @@
 package com.example.webbanhang.controller;
 
-import ch.qos.logback.core.model.Model;
-import com.example.webbanhang.model.Course;
+import org.springframework.ui.Model;
 import com.example.webbanhang.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
 @RequestMapping("/home1")
 public class HomeController1 {
@@ -17,6 +15,7 @@ public class HomeController1 {
     public String index() {return "home1";}
     @GetMapping("")
     public String index(Model model) {
-        model.addText()
+        model.addAttribute("listcourse",courseService.GetAll());
+        return "home1";
     }
 }
